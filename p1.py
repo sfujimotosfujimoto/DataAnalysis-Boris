9b28138a8cbad7256bb9d93390dca239c5a3dbba
S Fujimoto <unicoshun@gmail.com>
2017-12-27 14:42:02 +0900

    1st

---
+++ p1.py
@@ -0,0 +1,26 @@
+# 17
+"Hello world"
+type(-1)
+age = (25 + 75) * 3 / 10
+age
+
+nums = [1, 5, 10, 30]
+nums
+len(nums)
+
+menu = {
+    "Filet Mignon": 29.99,
+    "Big Mac": 3.99,
+    "Pizza": 0.99,
+    "Salmon": 29.99
+}
+menu["Pizza"]
+
+
+def convert_to_Fahrenheit(celsiusTemp):
+    product = celsiusTemp * 1.8
+    final = product + 32
+    return final
+
+
+convert_to_Fahrenheit(12)
